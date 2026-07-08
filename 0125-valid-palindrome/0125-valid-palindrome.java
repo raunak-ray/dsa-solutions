48eeafff033f99816b38b0446fe83cf88ad136e9
Raunak Ray <raunakray3260@gmail.com>
2026-07-08 22:24:06 +0530

    Time: 17 ms (12.42%), Space: 47.1 MB (15.36%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll(" ", "").replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int l = 0, r = s.length() - 1;
+
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
